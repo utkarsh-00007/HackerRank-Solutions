54c8cf5fb6afec8fccd509e20446bdceed347bce
Utkarsh Dwivedi <89937145+utkarsh-00007@users.noreply.github.com>
2022-02-01 21:15:31 +0530

    added 1st Solution

---
+++ WelcomeJava.java
@@ -0,0 +1,30 @@
+public class Solution1 {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.print("Hello, Java.");
+    }
+}
+/*
+Problem Statement 1:Welcome to Java!
+Welcome to the world of Java! In this challenge, we practice printing to stdout.
+
+The code stubs in your editor declare a Solution class and a main method. Complete the main method by copying the two lines of code below and pasting them inside the body of your main method.
+
+System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+Input Format
+
+There is no input for this challenge.
+
+Output Format
+
+You must print two lines of output:
+
+1-Print Hello, World. on the first line.
+2-Print Hello, Java. on the second line.
+Sample Output
+
+Hello, World.
+Hello, Java.
+ */
